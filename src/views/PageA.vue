89916ddab065cb1762fd814232dd1a1bc32eadd7
骆君亮 <1094267461@qq.com>
2022-06-02 11:05:27 +0800

    此次加入了一个div

--- src/views/PageA.vue
+++ src/views/PageA.vue
@@ -1,6 +1,7 @@
 <template>
 <router-view > sfakfalf</router-view><br>
 <div>我是PageA</div><br>
+<div>明天端午放假 </div>
 <router-view name="hh"></router-view>
 <router-link to="/sx1">陈明市傻逼啊 啊 SX</router-link><br>
 <router-link to="/sb1">点我SB</router-link><br>
